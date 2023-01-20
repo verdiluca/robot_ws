adbae91e822cc8648ee20e7652827a8622af5168
verdiluca <luca@tema.srl>
2023-01-20 17:51:29 +0100

    20 1 23

--- build/odrive_driver/build/lib/odrive_driver/odrive_node.py
+++ build/odrive_driver/build/lib/odrive_driver/odrive_node.py
@@ -1,12 +1,64 @@
 import rclpy
+import tf
+import math
+import time 
+import numpy as np
+import geometry_msgs
 from odrive.enums import *
 from rclpy.node import Node
+from std_msgs.msg import Char
+from std_msgs.msg import String
 from std_msgs.msg import Int32
 from std_msgs.msg import Int64
 from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from .odrive_command import ODriveController
 from sensor_msgs.msg import JointState
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import TwistStamped
+from scipy.spatial.transform import Rotation as R
+from rcl_interfaces.msg import ParameterDescriptor
+from tf2_ros.transform_broadcaster import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+
+
+
+
+
+demandx = 0.0
+demandz = 0.0
+pos0 = 0.0
+pos1 = 0.0
+pos0_diff = 0.0
+pos1_diff = 0.0
+pos0_old = 0.0
+pos1_old = 0.0
+pos0_mm_diff = 0.0
+pos1_mm_diff = 0.0
+phi = 0.0
+delta_th = 0.0
+
+x = 0.0
+y = 0.0
+dth = 0.0
+
+prev_update_time = time.time()
+current_time = time.time()
+
+
+
+def get_quaternion_from_euler(roll, pitch, yaw):
+  
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+  
+ 
+    return [qw, qx, qy, qz]
+
+
 
 #INIZIO CLASSE
 
@@ -22,6 +74,9 @@ class ODriveNode(Node):
         self.left_wheel_rpm = self.create_publisher(
             Int64, 'left_wheel_rpm', 10)
 
+        self.odom = self.create_publisher(
+            Odometry, 'odom', 10)
+
 
         self.Joint_State = self.create_publisher(
             JointState, 'joint_states', 10)
@@ -33,6 +88,25 @@ class ODriveNode(Node):
            Twist, '/cmd_vel', self.cmd_callback, 10)  
 
     def timer_callback(self):
+
+
+        global x
+        global y
+        global dth
+        global prev_update_time
+        global current_time
+        global demandx
+        global demandz
+        global pos0
+        global pos1
+        global pos0_diff
+        global pos1_diff
+        global pos0_old
+        global pos1_old
+        global pos0_mm_diff
+        global pos1_mm_diff
+        global phi
+        global delta_th
 
         msg3 = Int64()
         msg4 = Int64()
@@ -65,14 +139,91 @@ class ODriveNode(Node):
         joint_state_position.position = [pos0,pos1]
         # joint_state_velocity.velocity = [vel0,vel1]
         joint_state_position.header.stamp = self.get_clock().now().to_msg()
+        # joint_state_velocity.header.stamp = self.get_clock().now().to_msg()
 
         self.Joint_State.publish(joint_state_position)
-        self.Joint_State.publish(joint_state_velocity)
+        # self.Joint_State.publish(joint_state_velocity)
+
+        ###################################################################################################
+
+        vel = Int32()
+        msg = Float32()
+        msg2 = Twist()
+
+        self.broadcaster = TransformBroadcaster(self, 10)
+        self.odometry = Odometry()
+        odom_trans = TransformStamped()
+        odom_trans.header.frame_id = 'odom'
+        odom_trans.child_frame_id = 'base_link'
+        self.odometry.header.frame_id = 'odom'
+        self.odometry.child_frame_id = 'base_link'
+
+        pos0_diff = pos0 - pos0_old
+        pos1_diff = pos1 - pos1_old
+        pos0_old = pos0
+        pos1_old = pos1
+
+        pos0_mm_diff = pos0_diff / 0.0058
+        pos1_mm_diff = pos1_diff / 0.0058
+
+
+        current_time = time.time()
+        phi = ((pos1_mm_diff - pos0_mm_diff) / 360)
+        dt = (current_time - prev_update_time)
+        #delta_th = (demandz) * dt
+        delta_th += phi
+
+        if delta_th >= 6.28 :
+            delta_th -= 6.28
+            
+        if delta_th <= (-6.28) :
+            delta_th += 6.28
+
+        delta_x = pos0 * math.cos(delta_th) #* dt
+        delta_y = pos1 * math.sin(delta_th) #* dt
+
+        if pos0_diff != 0 and pos1_diff !=0:
+            x += delta_x
+            y += delta_y
+            dth = delta_th
+
+        quat_tf = get_quaternion_from_euler(0,0,dth)
+        msg_quat = Quaternion(w=quat_tf[0], x=quat_tf[1], y=quat_tf[2], z=quat_tf[3])
+        print(x)
+
+
+        self.odometry.pose.pose.position.x = x/1000
+        self.odometry.pose.pose.position.y = y/1000
+        self.odometry.pose.pose.position.z = 0.0
+        self.odometry.pose.pose.orientation = msg_quat
+        self.odometry.twist.twist.linear.x = ((pos0_mm_diff + pos1_mm_diff) /2)/10
+        self.odometry.twist.twist.linear.y = 0.0
+        self.odometry.twist.twist.linear.z = 0.0
+        self.odometry.twist.twist.angular.x = 0.0
+        self.odometry.twist.twist.angular.y = 0.0
+        self.odometry.twist.twist.angular.z = ((pos1_mm_diff - pos0_mm_diff) /360)*100
+        self.odometry.header.stamp = self.get_clock().now().to_msg()
+        self.odom.publish(self.odometry)
+
+        odom_trans.header.stamp = self.get_clock().now().to_msg()
+        odom_trans.transform.translation.x = self.odometry.pose.pose.position.x
+        odom_trans.transform.translation.y = self.odometry.pose.pose.position.y
+        odom_trans.transform.translation.z = self.odometry.pose.pose.position.z
+        odom_trans.transform.rotation = self.odometry.pose.pose.orientation 
+        self.broadcaster.sendTransform(odom_trans)
+
+
+        ######################################################################################################à
+
 
     def cmd_callback(self, msg2):
 
         self.odrv0.command_velocity(0, msg2.linear.x + msg2.angular.z)
         self.odrv0.command_velocity(1, msg2.linear.x - msg2.angular.z)
+
+
+
+
 
 
 def main(args=None):
--- src/vp_one/launch/launch_sim.launch.py
+++ src/vp_one/launch/launch_sim.launch.py
@@ -25,11 +25,11 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    #          )
+    #Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -42,6 +42,6 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        # gazebo,
+        gazebo,
         spawn_entity,
     ])
